ff9a1973ecc62dc4f7f12277e60e412d1d346e32
Erith <66786887+erith-c@users.noreply.github.com>
2020-06-30 17:52:19 -0400

    Added `ALS Class Library`
    Uploaded Class Library Visual Studio project from APC local computer.

---
+++ ALS Class Library/als-core.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ALS
+{
+    namespace APC
+    {
+        public class DataRecord
+        {
+            // Properties
+            public string Name { get; }
+            public string Source { get; set; }
+            public string CellRef { get; set; }
+            public string Value { get; set; }
+            // Methods
+
+            // Constructors
+            public DataRecord()
+            {
+                Name = "";
+                Source = "";
+                CellRef = "";
+                Value = "";
+            }
+        }
+    }
+}
